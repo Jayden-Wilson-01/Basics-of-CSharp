a9fecbcd4fd4c6c98ba54e6cb4b553496e0f450f
Jayden Wilson <jayden@tabber.co.uk>
2024-10-12 16:00:43 +0100

    Lesson 1

---
+++ Basics-of-CSharp/Lesson1/Exercises.cs
@@ -0,0 +1,35 @@
+﻿/* Lesson 1 - Exercises
+ * Jayden Wilson
+ * 12 Oct 2024
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics_of_CSharp.Lesson1
+{
+    public static class Exercises
+    {
+        /// <summary>
+        /// A method to display "Hello World" to console. 
+        /// </summary>
+        public static void HelloWorld()
+        {
+            //Writes "Hello World" to Console.
+            Console.WriteLine("Hello");
+            Console.WriteLine("World!");
+        }
+
+        /// <summary>
+        /// A method to display a message with "" Included to console.
+        /// </summary>
+        public static void Message()
+        {
+            //Writes a message to Console with "" included
+            Console.Write("The teacher says \"I'm a good programmer!\" ");
+        }
+    }
+}
---
+++ Basics-of-CSharp/Project.md
@@ -0,0 +1,10 @@
+﻿# Basics of C# (CSharp)
+
+## Lesson 1 - Getting Started
+
+### Exercises 
+
+- Method 1: `public static void HelloWorld()` 
+- Method 2: `public static void Message()`
+
+
